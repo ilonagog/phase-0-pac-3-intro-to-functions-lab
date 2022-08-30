113faa7b35185b1033676a8141da8f4be194614d
ilona <gogiashvili.ilona@gmail.com>
2022-08-30 19:11:08 -0400

    commit tree e98511d97d65636334c7de5aa70bec1816028564

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+    // todo
+}
+
+function shout(string) {
+    return string;
+}
+"Hello!".toUpperCase(); // 'HELLO!'
+
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string;
+}
+'HELLO'.toLowerCase(); // 'Hello'
+
+const string = "Hello!";
+
+console.log(string);
+console.log(string.toUpperCase());
+
+console.log(string === string.toUpperCase());
